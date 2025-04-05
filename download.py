6e6839ec167bd3972e08b3daf96c1a16ffce9903
BorhanPetgar <borhanpetgar40@gmail.com>
2025-04-05 10:19:32 +0330

    Update download.py

--- download.py
+++ download.py
@@ -11,12 +11,14 @@ import gdown
 # https://drive.google.com/drive/folders/1qgdD-D3CgyDa4SBp9a-5H4-rOr0vF5Fv
 # https://drive.google.com/drive/folders/1qgdD-D3CgyDa4SBp9a-5H4-rOr0vF5Fv
 # url = 'https://drive.google.com/uc?id=16IJeYqt9oHbqnSI9m2nTXcxQWNXCfiGb'
-# output = 'val2014.zip'
-# gdown.download(url, output, quiet=False)
+url = 'https://drive.google.com/uc?id=1Fn-cUESMMF1pQy8Xff-vPQvXJdZoUlP3'
+output = 'fss.zip'
+gdown.download(url, output, quiet=False)
 
 # OPTION 2: Download an entire folder
-folder_id = "1qgdD-D3CgyDa4SBp9a-5H4-rOr0vF5Fv"  # This is the ID from your commented URL
-output_folder = "checkpoints"  # Where to save the folder contents
+# folder_id = "1qgdD-D3CgyDa4SBp9a-5H4-rOr0vF5Fv"  # This is the ID from your commented URL
+# folder_id = '12eoVUGtAtuMHYLeyuyi2t1O8Ljxly4cI'
+# output_folder = "sent_dataset"  # Where to save the folder contents
 
-# Download the entire folder
-gdown.download_folder(id=folder_id, output=output_folder, quiet=False)
+# # Download the entire folder
+# gdown.download_folder(id=folder_id, output=output_folder, quiet=False)
